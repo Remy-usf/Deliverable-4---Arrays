f3d80b8d5af20f87430c9f083d67ec1d7708a246
fitca <fitca@DESKTOP-V8CSPQE>
2023-02-09 11:21:17 -0500

    Add project files.

---
+++ Deliverable 4 - Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Deliverable_4___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibNumbers = new int[25];
+            fibNumbers[0] = 0;
+            fibNumbers[1] = 1;
+
+            for (int i = 2; i < 25; i++)
+            {
+                fibNumbers[i] = fibNumbers[i - 2] + fibNumbers[i - 1];
+            }
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fibNumbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
